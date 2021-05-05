f2c3ded7b9b513bd2c6c396c98c4edcf75e0688a
shanazirah <83662047+shanazirah@users.noreply.github.com>
2021-05-05 17:33:40 +0200

    S7.R

--- Code Projet provisoire.R
+++ Code Projet provisoire.R
@@ -6,18 +6,9 @@ library(dplyr)
 library(readr)
 library(tidyverse)
 
-simu_graph <- function(Gender, Degree){
-  obs <- cbind(Gender, Degree)
-  obs_f <- obs[,2][which (Gender == 1)]
-  obs_h <- obs[,2][which (Gender == 0)]
-  
-  return(list(new_graph = obs, moy_f = mean(obs_f), moy_h = mean(obs_h)))
-}
-
 # ---------- Importation des données ----------
 
 data <- read_csv(file = "persons.csv", 
-
          col_types = cols(
            'gender' = col_character(),
            'degrees' = col_integer()
@@ -28,70 +19,67 @@ data <- read_csv(file = "persons.csv",
 head(data)
 summary(data)
 
+# Boxplot des données 
 ggplot(data, aes(x = gender, y = degrees, fill = gender)) +
-  geom_boxplot(alpha=.3, outlier.shape = NA) +
-  geom_jitter(width=.1, size=2) +
+  geom_boxplot(alpha = .3, outlier.shape = NA) +
+  geom_jitter(width = .1, size = 2) +
   theme_classic() +
   scale_fill_manual(values = c("firebrick", "dodgerblue"))
 
-# 1 = femme ; 0 = homme
-Gender <- as.matrix(sapply(data$gender[1:100], function(x) {return (1 * (x == "Female"))}))
-
-Degree <- as.matrix(sapply(data$degrees[1:100], function(x) as.numeric(x)))
-
-W <- data$degrees[which (Gender == 1)]
-M <- data$degrees[which (Gender == 0)]
+# Visualisation des données par classe
+W <- data$degrees[which (data$gender == "Female")]
+M <- data$degrees[which (data$gender == "Male")]
 
 mean(W)
 mean(M)
 
-#tib = tibble(x = as.character(Gender), y = Degree)
-
 # ---------- Visualisation des rangs des données ----------
 data = data %>%
-  mutate(rangs = rank(Degree))
+mutate(rangs = rank(data$degrees))
 data
 
+# Boxplot des rangs des données
 ggplot(data, aes(x = gender, y = rangs, fill = gender)) +
-  geom_boxplot(alpha=.3, outlier.shape = NA) +
-  geom_jitter(width=.1, size=2) +
+  geom_boxplot(alpha = .3, outlier.shape = NA) +
+  geom_jitter(width = .1, size = 2) +
   theme_classic() +
   scale_fill_manual(values = c("firebrick", "dodgerblue"))
 
-data_summary = data %>% 
-  group_by(gender)%>% 
-  summarise(n = n(),
-            rank_mean = mean(rangs),
-            rank_sum_0 = n * (n + 1)/2,
-            rank_sum = sum(rangs),
-            M_U = rank_sum - rank_sum_0,
-            .groups="drop")
+# Résumé des valeurs des statistiques 
+data_summary = data %>% group_by(gender)%>% summarise(n = n(),
+                                                      rank_mean = mean(rangs),
+                                                      rank_sum_0 = n * (n + 1)/2,
+                                                      rank_sum = sum(rangs),
+                                                      M_U = rank_sum - rank_sum_0,
+                                                      .groups = "drop")
 data_summary
 
 # ---------- Test de Mann-Whitney ----------
 montest = wilcox.test(degrees ~ gender, data = data)
 montest
 
-
-# ---------- Test par les modèles nuls ----------
+# ---------- Test par les modèles nuls complexes ----------
 n <- 1000
-diff_moy <- vector('list', length = n)
+diff_moy <- numeric(n)
 
 for (i in 1:n){
-  Sexe <- sample(Gender, 100, replace = FALSE)
-  result <- simu_graph(Sexe, Degree)
-  diff_moy[[i]] <- result$moy_f - result$moy_h
+  Sexe <- sample(data$gender, 100, replace = FALSE)
+  moy_f = mean(data$degrees[which (Sexe == "Female")])
+  moy_h = mean(data$degrees[which (Sexe == "Male")])
+  diff_moy[i] <- moy_f - moy_h
 }
 
-dm <- data.frame(diff_mean = unlist(diff_moy))
+# ---------- Histogramme des différences de moyennes ----------
+dm <- data.frame(diff_mean = diff_moy)
 
 ggplot(dm, aes(x = diff_mean)) +
   geom_histogram(color = "black", fill = "blue", alpha = .4) +
-  geom_vline(color = "red",lwd = 1,lty = 2, xintercept = 1.033654) +
-  theme_classic()+
+  geom_vline(color = "red", lwd = 1, lty = 2, xintercept = 1.033654) +
+  theme_classic() +
   ggtitle("Différences de moyennes sur 1000 échantillons simulés")
 
-pval <- 100 * sum(unlist(diff_moy) > (mean(W) - mean(M))) / 1000
-pval
+# ---------- Calcul de la p-valeur ----------
+p_val <- 100 * (sum(diff_moy > (mean(W) - mean(M))) / n)
+p_val
 
 
